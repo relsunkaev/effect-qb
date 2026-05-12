23407775bcc4982be9ee284ce468a40f6656b2f0
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-12 08:55:25 -0700

    test(mysql): reproduce json type name casing

--- test/public/types/mysql-json-types.ts
+++ test/public/types/mysql-json-types.ts
@@ -37,17 +37,25 @@ const setWithoutCreateExpr = J.json.set(docs.payload, suitePath, "12A", {
 const tagsExpr = J.json.get(docs.payload, J.json.path(J.json.key("profile"), J.json.key("tags")))
 const tagsKeysExpr = J.json.keys(tagsExpr)
 const scalarLengthExpr = J.json.length(scalarDocs.payload)
+const objectTypeExpr = J.json.typeOf(docs.payload)
+const scalarTypeExpr = J.json.typeOf(scalarDocs.payload)
 
 type SetWithoutCreate = E.RuntimeOf<typeof setWithoutCreateExpr>
 type TagsKeys = E.RuntimeOf<typeof tagsKeysExpr>
 type ScalarLength = E.RuntimeOf<typeof scalarLengthExpr>
+type ObjectType = E.RuntimeOf<typeof objectTypeExpr>
+type ScalarType = E.RuntimeOf<typeof scalarTypeExpr>
 
 const tagsKeys: TagsKeys = null
 const scalarLength: ScalarLength = 1
+const objectType: ObjectType = "OBJECT"
+const scalarType: ScalarType = "STRING"
 declare const setWithoutCreate: SetWithoutCreate
 
 void tagsKeys
 void scalarLength
+void objectType
+void scalarType
 
 // @ts-expect-error createMissing: false should not add a missing object key
 setWithoutCreate.profile.address.suite
@@ -64,3 +72,5 @@ J.json.delete(docs.payload, tagWildcardPath)
 void setWithoutCreateExpr
 void tagsKeysExpr
 void scalarLengthExpr
+void objectTypeExpr
+void scalarTypeExpr
