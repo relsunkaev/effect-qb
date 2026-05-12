5bf5d56f8ddef474d52e03ec4054111f9835cb4f
Ramazan Elsunkaev <relsunkaev@outlook.com>
2026-05-12 09:16:12 -0700

    test(json): reproduce mutation value widening

--- test/public/types/mysql-json-types.ts
+++ test/public/types/mysql-json-types.ts
@@ -41,6 +41,26 @@ const lastPairValueExpr = J.json.get(
   docs.payload,
   J.json.path(J.json.key("profile"), J.json.key("pair"), J.json.index(-1))
 )
+const setLastPairValueExpr = J.json.set(
+  docs.payload,
+  J.json.path(J.json.key("profile"), J.json.key("pair"), J.json.index(-1)),
+  true
+)
+const deleteLastPairValueExpr = J.json.delete(
+  docs.payload,
+  J.json.path(J.json.key("profile"), J.json.key("pair"), J.json.index(-1))
+)
+const insertBeforeLastPairValueExpr = J.json.insert(
+  docs.payload,
+  J.json.path(J.json.key("profile"), J.json.key("pair"), J.json.index(-1)),
+  true
+)
+const insertAfterLastPairValueExpr = J.json.insert(
+  docs.payload,
+  J.json.path(J.json.key("profile"), J.json.key("pair"), J.json.index(-1)),
+  true,
+  { insertAfter: true }
+)
 const scalarLengthExpr = J.json.length(scalarDocs.payload)
 const objectTypeExpr = J.json.typeOf(docs.payload)
 const scalarTypeExpr = J.json.typeOf(scalarDocs.payload)
@@ -49,12 +69,20 @@ const wildcardPath = J.json.path(J.json.key("profile"), J.json.key("tags"), J.js
 type SetWithoutCreate = E.RuntimeOf<typeof setWithoutCreateExpr>
 type TagsKeys = E.RuntimeOf<typeof tagsKeysExpr>
 type LastPairValue = E.RuntimeOf<typeof lastPairValueExpr>
+type SetLastPairValue = E.RuntimeOf<typeof setLastPairValueExpr>
+type DeleteLastPairValue = E.RuntimeOf<typeof deleteLastPairValueExpr>
+type InsertBeforeLastPairValue = E.RuntimeOf<typeof insertBeforeLastPairValueExpr>
+type InsertAfterLastPairValue = E.RuntimeOf<typeof insertAfterLastPairValueExpr>
 type ScalarLength = E.RuntimeOf<typeof scalarLengthExpr>
 type ObjectType = E.RuntimeOf<typeof objectTypeExpr>
 type ScalarType = E.RuntimeOf<typeof scalarTypeExpr>
 
 const tagsKeys: TagsKeys = null
 const lastPairValue: LastPairValue = 1
+declare const setLastPairValue: SetLastPairValue
+declare const deleteLastPairValue: DeleteLastPairValue
+declare const insertBeforeLastPairValue: InsertBeforeLastPairValue
+declare const insertAfterLastPairValue: InsertAfterLastPairValue
 const scalarLength: ScalarLength = 1
 const objectType: ObjectType = "OBJECT"
 const scalarType: ScalarType = "STRING"
@@ -62,6 +90,12 @@ declare const setWithoutCreate: SetWithoutCreate
 
 void tagsKeys
 void lastPairValue
+const setLastPairTail: boolean = setLastPairValue.profile.pair[1]
+const deleteLastPairLength: 1 = deleteLastPairValue.profile.pair.length
+const insertBeforeLastPairMiddle: boolean = insertBeforeLastPairValue.profile.pair[1]
+const insertBeforeLastPairTail: number = insertBeforeLastPairValue.profile.pair[2]
+const insertAfterLastPairPreviousTail: number = insertAfterLastPairValue.profile.pair[1]
+const insertAfterLastPairTail: boolean = insertAfterLastPairValue.profile.pair[2]
 void scalarLength
 void objectType
 void scalarType
@@ -87,6 +121,16 @@ J.json.stripNulls(docs.payload)
 void setWithoutCreateExpr
 void tagsKeysExpr
 void lastPairValueExpr
+void setLastPairValueExpr
+void deleteLastPairValueExpr
+void insertBeforeLastPairValueExpr
+void insertAfterLastPairValueExpr
+void setLastPairTail
+void deleteLastPairLength
+void insertBeforeLastPairMiddle
+void insertBeforeLastPairTail
+void insertAfterLastPairPreviousTail
+void insertAfterLastPairTail
 void scalarLengthExpr
 void objectTypeExpr
 void scalarTypeExpr
